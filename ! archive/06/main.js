f9f67252073b1c4898c6f9201d108b5624301891
Alecto <fomenkoandrey1978@gmail.com>
2024-04-11 21:24:10 +0300

    Add alternative loop implementations in main.js

---
+++ ! archive/06/main.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-restricted-syntax */
+const arr = [ 1, 2, 3, 4, 5 ]
+
+// ! forEach
+arr.forEach(val => console.log(val))
+// arr.forEach((val, index, array) => console.log(index, val, array))
+
+// ! for of
+// for (const item of arr) {
+//   console.log(item)
+// }
+
+// ! for
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i])
+// }
